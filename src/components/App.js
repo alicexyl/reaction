88399321d20cfca22db04d93cbf5f3cc81f4b8c5
Alice Liu <alice.xyl@gmail.com>
2019-05-27 21:19:16 -0700

    Added new component for pushing user-inputted message.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import PublishMessage from './PublishMessage';
 
 class App extends Component {
    render() {
         return (
             <div>
-                React App
+                <h2>Reaction</h2>
+                <hr />
+                <PublishMessage />
             </div>
         );
     }
